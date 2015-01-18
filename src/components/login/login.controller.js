12525ce6fee1e23b10720bbd709a153a7289bb5f
Jeremy Robles <jeremy.robles56@gmail.com>
2015-01-18 15:14:39 -0800

    update test

---
+++ src/components/login/login.controller.js
@@ -0,0 +1,3 @@
+/**
+ * Created by jeremyrobles on 1/18/15.
+ */
---
+++ src/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('desktop')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
